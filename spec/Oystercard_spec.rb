56217162bfb076f5616e6f62346efb268800bb5c
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-05 15:09:41 +0100

    Creating test for balance and passing that test.

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,2 +1,11 @@
-describe do
+require 'Oystercard'
+
+describe Oystercard do
+
+  let(:new_card) {Oystercard.new}
+
+  it 'expects card balance to be 0' do
+    expect(new_card.balance).to eq(0)
+  end
+
 end
